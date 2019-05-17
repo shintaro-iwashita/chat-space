7af788a4f3e53abbf2ec3c794dd1ac4778be060a
shintaro-iwashita <shintaro@Shintaro-mac.local>
2019-05-17 21:28:47 +0900

    Add messages ajax

---
+++ app/assets/javascripts/messege.js
@@ -0,0 +1,69 @@
+$(document).on('turbolinks:load', function() { 
+function buildHTML(message){
+  if ( message.image ) {
+  var html =
+  `<div class="info" data-message-id=${message.id}>
+  <div class="upper-info">
+    <div class="upper-info__user-name">
+      ${message.user_name}
+    </div>
+    <div class="upper-info__date">
+      ${message.date}
+    </div>
+  </div>
+  <div class="text-message">
+    <p class="text-message__content">
+      ${message.content}
+    </p>
+  </div>
+  <img src=${message.image} >
+</div>`
+
+  return html;
+  } else {
+  var html = `<div class="info" data-message-id=${message.id}>
+  <div class="upper-info">
+    <div class="upper-info__user-name">
+      ${message.user_name}
+    </div>
+    <div class="upper-info__date">
+      ${message.date}
+    </div>
+  </div>
+  <div class="text-message">
+    <p class="text-message__content">
+      ${message.content}
+    </p>
+  </div>
+</div>`
+  
+  return html;
+  };
+  }
+
+  $('.new-message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: 'json',
+  processData: false,
+  contentType: false
+  })
+  .done(function(data){
+  var html = buildHTML(data);
+  $('.messages').append(html);
+  $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  $('form')[0].reset();
+  })
+  .fail(function(){
+  alert('error');
+  })
+  .always(() => {
+    $(".new-message__submit-btn").removeAttr("disabled");
+    });
+  });
+});
