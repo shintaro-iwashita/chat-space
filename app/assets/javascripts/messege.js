e5a64117636eddcdc043e9f2fed6c02fb8c648d6
shintaro-iwashita <shintaro@Shintaro-mac.local>
2019-05-18 17:23:37 +0900

    Console.log 削除

--- app/assets/javascripts/messege.js
+++ app/assets/javascripts/messege.js
@@ -55,8 +55,6 @@ function buildHTML(message){
   contentType: false
   })
   .done(function(data){
-    console.log(data.id)
-    console.log(data.user_name)
   var html = buildHTML(data);
   $('.messages').append(html);
   $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
